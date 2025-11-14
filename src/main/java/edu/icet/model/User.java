97de28f9704728932c104ec9d96a0a09fb9ab85e
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2025-11-14 14:48:08 +0530

    feat: add User model class with fields for id, name, username, and email

---
+++ src/main/java/edu/icet/model/User.java
@@ -0,0 +1,15 @@
+package edu.icet.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private int id;
+
+    private String name;
+
+    private  String userName;
+
+    private  String email;
+}
