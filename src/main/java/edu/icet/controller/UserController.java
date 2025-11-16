ff8a9c8dc10c9eba5fef439caca99b4184f4fc3a
pawara-mmns <pawarasamarawickrama@gmail.com>
2025-11-16 12:26:25 +0530

    feat: add UserController for handling user-related API requests

---
+++ src/main/java/edu/icet/controller/UserController.java
@@ -0,0 +1,30 @@
+package edu.icet.controller;
+
+import edu.icet.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import edu.icet.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/test")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userService.getAllUsers();
+    }
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable int id){
+        return userService.getUserById(id);
+    }
+
+}
